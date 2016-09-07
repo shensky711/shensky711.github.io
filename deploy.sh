9e75e4f00b25116ef9fe2119c4a68c47e78cd5ce
shensky711 <shensky711@qq.com>
2016-09-07 12:28:42 +0800

    modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -34,7 +34,7 @@ else
 	
 	git push -u origin ${BRANCH_SITE}
 	git push -u origin ${BRANCH_SOURCE}
-	git push -u coding coding-pages
+	git push -u coding ${BRANCH_SITE}:coding-pages
 	
 	echo "deploy succeed"
 fi
